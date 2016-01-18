b487ad5ba162e1a714fabf95257e3672cf9475fb
Kevin Simper <kevin.simper@gmail.com>
2016-01-18 15:41:46 +0100

    Bind the values to the action

--- components/Category/index.js
+++ components/Category/index.js
@@ -3,15 +3,15 @@ import styles from './style.scss'
 import Card from '../Card'
 
 export default class Category extends React.Component {
-  onClick() {
-    this.props.onCardClick('category', 'question')
+  onClick(category, question) {
+    this.props.onCardClick(category, question)
   }
   render() {
     return (
       <div className={styles.Category}>
         <div className={styles.Header}>{this.props.data.name}</div>
         {this.props.data.questions.map((item, key) => {
-          return <Card key={key} onClick={this.onClick.bind(this)}>{item.points}</Card>
+          return <Card key={key} onClick={this.onClick.bind(this, item, this.props.data.name)}>{item.points}</Card>
         })}
       </div>
     )
